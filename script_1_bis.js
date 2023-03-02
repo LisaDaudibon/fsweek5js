488013f1cc1e66f528b124e81cd60db81d6c9cc8
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-02 16:12:16 +0100

    THE BEGINNING

---
+++ script_1_bis.js
@@ -0,0 +1,4 @@
+
+nom = prompt('Quel est ton nom, jeune padawan ?');
+
+console.log(`Hello ${nom} !`)
---
+++ script_3.js
@@ -0,0 +1,9 @@
+
+numb = prompt ("Donne moi un chiffre, espèce de Valentin :")
+Number(`${numb}`)
+
+for(let count = 0; count <=numb; count++){
+  let space = `""` *= numb-1;
+  console.log(`${space}`)
+  
+}
